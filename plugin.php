70293da17829737c1f4d4029337cc3de011c6cf8
shafisultan <shafisultan@utmuser162-11.wireless.utoronto.ca>
2016-08-18 17:22:53 -0400

    work plugin

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/* Plugin Name: plugin-shafi
+Plugin URI: https://www.theindividualist.me
+Description: Plugin for Assignment 2 for CCT460
+Author: Shafi Sultan
+Author URI: https://www.theindividualist.me
+Version: 1.0
+*/
+
+/* echo "# plugin-shafi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/shafisultan/plugin-shafi.git
+git push -u origin master */
---
+++ readme.txt
@@ -0,0 +1,7 @@
+This is a readme file for Plugin
+echo "# plugin-shafi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/shafisultan/plugin-shafi.git
+git push -u origin master
